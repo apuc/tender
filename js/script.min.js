99b1c0cd0d9207e74ed12ccbe80a76d559c1179a
gorotto <yastreb92@inbox.ru>
2016-08-10 17:21:17 +0300

    add-tabs

--- js/script.min.js
+++ js/script.min.js
@@ -64,6 +64,15 @@ $(document).ready(function () {
         start: 'top',
     });
 
+    $('.page__tabs_target').click(function(event) {
+      $('.page__tabs_target').removeClass('page__tabs_active');
+      $(this).addClass('page__tabs_active');
+      event.preventDefault();
+      var target = ($(this).data('tab'));
+      
+      $('.page__tabcontent').hide();
+      $("."+ target).show();
+    });
 
 
 });
